10ee2601f8041a59755e3b2ba0077c923d7cabf7
stan <stanislaus.dusek@gmail.com>
2024-12-11 19:24:23 +0100

    day 6 part 1

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -2,8 +2,11 @@
 using AdventOfCode2024.Solutions;
 
 Console.WriteLine("AOC 2024 | @stanuwu\n");
+/*
 AocSolver.Solve(new Day1());
 AocSolver.Solve(new Day2());
 AocSolver.Solve(new Day3());
 AocSolver.Solve(new Day4());
 AocSolver.Solve(new Day5());
+*/
+AocSolver.Solve(new Day6());
--- AdventOfCode2024/Solutions/Day6.cs
+++ AdventOfCode2024/Solutions/Day6.cs
@@ -35,6 +35,66 @@ public class Day6 : AocBase<int[][]>
 
     public override string Part1(int[][] input)
     {
-        return base.Part1(input);
+        int gx = -1;
+        int gy = -1;
+        int go = 0;
+
+        int width = input[0].Length;
+        int height = input.Length;
+
+        for (int y = 0; y < height; y++)
+        for (int x = 0; x < width; x++)
+        {
+            if (input[y][x] == 2)
+            {
+                gx = x;
+                gy = y;
+                input[y][x] = 0;
+                break;
+            }   
+        }
+
+        HashSet<int> visited = new HashSet<int>();
+        
+        while (true)
+        {
+            visited.Add(gx + (gy << 10));
+            int nextX = gx;
+            int nextY = gy;
+            switch (go)
+            {
+                case 0:
+                    nextY--;
+                    break;
+                case 1:
+                    nextX++;
+                    break;
+                case 2:
+                    nextY++;
+                    break;
+                case 3:
+                    nextX--;
+                    break;
+            }
+
+            if (nextX < 0 || nextX >= width || nextY < 0 || nextY >= height)
+            {
+                break;
+            }
+
+            switch (input[nextY][nextX])
+            {
+                case 0:
+                    gx = nextX;
+                    gy = nextY;
+                    break;
+                case 1:
+                    go++;
+                    if (go > 3) go = 0;
+                    break;
+            }
+        }
+        
+        return visited.Count.ToString();
     }
 }
